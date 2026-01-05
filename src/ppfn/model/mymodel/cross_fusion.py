c429e8873952d6718fd1479b316f2e6a8706c04c
timruhkopf <timruhkopf@gmail.com>
2026-01-05 16:11:46 +0100

    [feature]  Cross fusion V2 a new delta C stream learning and Q(A), K(B), V(B)
    [minor refactor] cross fusion validation of the fwd

--- src/ppfn/model/mymodel/cross_fusion.py
+++ src/ppfn/model/mymodel/cross_fusion.py
@@ -4,7 +4,7 @@ from torch.nn import MultiheadAttention
 import torch.nn.functional as F
 import mlflow
 
-from typing import Dict
+from typing import Dict, Tuple
 
 from ppfn.trainer.callbacks.abstract_callback import AbstractCallback
 
@@ -45,41 +45,46 @@ class CrossFusion(nn.Module):
         nn.init.constant_(self.norm.weight, 1)
         nn.init.constant_(self.norm.bias, 0)
 
-    def forward(self, x, *args, **kwargs):
+    def validate_forward_args(self, x, *args, **kwargs) -> Tuple[int, int]:
         if "single_eval_pos" in kwargs:
             single_eval_pos = kwargs["single_eval_pos"]
         else:
-            # if the pfn does not communicate this argument (e.g. to a linear layer)
+            # if the pfn does not communicate this argument (e.g. to a linear layer),
+            # then the model should communicate it to the interleaved layers directly!
             single_eval_pos = self.single_eval_pos
 
+        assert single_eval_pos is not None, (
+            "single_eval_pos must be provided during training"
+        )
+
+        # we expect that we get pairs of tasks, i.e. A (target task tensor untainted),
+        # B (related tasks untainted), C (related tasks conditional predictions to be updated)
         B = x.shape[1]
-        if self.training:
-            # we expect that we get pairs of tasks, i.e. A (target task tensor untainted),
-            # B (related tasks untainted), C (related tasks conditional predictions to be updated)
-            assert B % 3 == 0, (
-                "In training mode, batch size must be multiple of 3 (A,B,C task triplets)"
-            )
-            assert single_eval_pos is not None, (
-                "single_eval_pos must be provided during training"
-            )
-            R = B // 3  # number related tasks
-            Q = x[ :, :R, : ]
-            # (stream A) key: target task marginal predictions (untainted)
-            K = x[ :, R : 2 * R, : ]
-            # (stream B) value: related tasks' marginal predictions (untainted)
-            V = x[ :, 2 * R :, : ]
-            # (stream C) query: related tasks' conditional predictions (to be updated)
+        assert B % 3 == 0, (
+            "In training mode, batch size must be multiple of 3 (A,B,C task triplets)"
+        )
+        return B, single_eval_pos
 
-        else:
-            # during evaluation we have only one target task and R related tasks (|A|=1), B, C as before
-            R = (B - 1) // 2  # number related
+    def forward(self, x, *args, **kwargs):
+        B, single_eval_pos = self.validate_forward_args(x, *args, **kwargs)
+        
+        R = B // 3  # number related tasks
+        Q = x[ :, :R, : ]
+        # (stream A) key: target task marginal predictions (untainted)
+        K = x[ :, R : 2 * R, : ]
+        # (stream B) value: related tasks' marginal predictions (untainted)
+        V = x[ :, 2 * R :, : ]
+        # (stream C) query: related tasks' conditional predictions (to be updated)
 
-            # (stream A) key: target task marginal predictions (untainted)
-            Q = x[:, :1, :].expand( -1, R, -1)
-            # (stream B) value: related tasks' marginal predictions (untainted)
-            K = x[ :, 1 : R + 1, : ]
-            # (stream C) query: related tasks' conditional predictions (to be updated)
-            V = x[ :, R + 1 :, : ]
+        # during evaluation we have only one target task and R related tasks (|A|=1), B, C as before
+        #     R = (B - 1) // 2  # number related
+
+        #     # (stream A) key: target task marginal predictions (untainted)
+        #     Q = x[:, :1, :].expand( -1, R, -1)
+        #     # (stream B) value: related tasks' marginal predictions (untainted)
+        #     K = x[ :, 1 : R + 1, : ]
+        #     # (stream C) query: related tasks' conditional predictions (to be updated)
+        #     V = x[ :, R + 1 :, : ]
 
         # Handle the train/test split
         # we only want to attend to the train set of the target task
@@ -108,12 +113,54 @@ class CrossFusion(nn.Module):
         )  # train + test updated conditionals
 
         # reconstruct the full (partially) updated output
-        if self.training:
-            y = torch.cat([x[:, : 2 * R, :], conditional], dim=1)
-        else:
-            y = torch.cat([x[:, : R + 1, :], conditional], dim=1)
+        output = torch.cat([x[:, : 2 * R, :], conditional], dim=1)
+        # eval:
+        #     output = torch.cat([x[:, : R + 1, :], conditional], dim=1)
 
-        return y
+        return output
+    
+class CrossFusionV2(CrossFusion):
+    def forward(self, x, *args, **kwargs):
+        B, sep = self.validate_forward_args(x, *args, **kwargs)
+        
+        R = B // 3  # number related tasks
+
+        # Stream (A) query target task marginal predictions (untainted)
+        Q = x[ :, : R , : ]
+        
+        # Stream (B) key related tasks' marginal predictions (untainted)
+        K = x[ :, R : 2 * R, : ]
+
+        # Stream (B) Value: what we want to extract from the related tasks based on 
+        # the learned kernel(A, B) * B, which will give us \Delta C, that we can add to C.
+        V = x[ :, R: 2 * R , : ] # Key Difference to CrossFusion: 
+
+        # Stream (C): last' layer's conditional predictions (to be updated)
+        C = x[ :, 2 * R :, : ]  
+
+        # Handle the train/test split
+        Q_train, Q_test = Q[:sep, :, :], Q[sep:, :, :]
+        K_train = K[:sep, :, :]#, K[sep:, :, :] for the test part (that we don't use)
+        V_train = V[:sep, :, :]#, V[sep:, :, :] 
+        C_train, C_test = C[:sep, :, :], C[sep:, :, :]
+
+        # already includes the skip connection from the original "C" Stream component
+        train_update = self.norm(self.cross_train(Q_train, K_train, V_train)[0]) + C_train
+        test_update = self.norm(self.cross_test(Q_test, K_train, V_train)[0]) + C_test
+
+        if self.use_gain:
+            gain = torch.sigmoid(self.gain)
+            train_update = train_update * gain
+            test_update = test_update * gain
+
+        conditional = torch.cat(
+            [train_update, test_update], dim=0
+        )  # train + test updated conditionals
+
+        # combine the untainted streams A, B with the updated conditional stream C
+        output = torch.cat([x[:, : 2 * R, :], conditional], dim=1)
+
+        return output
 
 
 class CrossFusionLossCallback(AbstractCallback):
@@ -126,7 +173,9 @@ class CrossFusionLossCallback(AbstractCallback):
 
         we need to replicate the targets to meet the model's output shape.
         
-        This is a temporary fix for the first iterations without an aggregation module!
+        This is a temporary fix: the original batch presented to the model has 
+        only the independent batch items. the model however parses the batch into 
+        three streams (A,B,C), so we need to replicate the targets accordingly.
 
         :param batch: The input batch containing single_eval_pos
         :param output: The model output tensor
@@ -134,21 +183,13 @@ class CrossFusionLossCallback(AbstractCallback):
 
         """
         B = output.shape[1]
+        R = B // 3
+
+        parser = self.trainer.model.parse_train_batch if self.trainer.model.training else self.trainer.model.parse_eval_batch
+
+        b = parser(batch)
         
-        # we need to repeat the target
-        if self.trainer.model.training:
-            R = B // 3
-            b = self.trainer.model.parse_train_batch(
-                batch
-            )  # to ensure the batch is in the right format
-
-        else: 
-            R = (B - 1) // 2
-            b = self.trainer.model.parse_eval_batch(
-                batch
-            )  # to ensure the batch is in the right format
-
-        return {"targets": b.y[batch.single_eval_pos :, ...]}  # loss on Stream C only
+        return {"targets": b.y[batch.single_eval_pos :, ...]}  
 
     def on_loss_end(self, batch, output, targets, loss) -> Dict:
         """
@@ -157,10 +198,10 @@ class CrossFusionLossCallback(AbstractCallback):
 
         """
         B = output.shape[1]
-        R = B // 3 if self.trainer.model.training else (B - 1) // 2
+        R = B // 3
 
         unconditional_loss = loss[:, :R, ...]  # Stream A
-        conditional_loss = loss[:, 2 * R :, ...]  # Stream C
+        conditional_loss = loss[:, -R:, ...]  # Stream C
 
         # compute auxiliary metric: difference between unconditional and conditional outputs
         # we compute the nll loss difference (the loss is calculated already, so we just need to compute
@@ -171,7 +212,7 @@ class CrossFusionLossCallback(AbstractCallback):
 
         #  KL divergence between unconditional and conditional predictions?
         kl = F.kl_div(
-            F.softmax(output[:, 2 * R :, :], dim=-1),
+            F.softmax(output[:, -R:, :], dim=-1),
             F.softmax(output[:, :R, :], dim=-1),
             reduction="batchmean",
         ).detach().mean().cpu().item()
@@ -179,7 +220,7 @@ class CrossFusionLossCallback(AbstractCallback):
         return {
             "kl_div_uncond_cond": kl,
             "nll_diff_uncond_cond": nll_diff,  # loss on Stream C only
-            "loss": loss[ :, 2 * R : ], 
+            "loss": loss[ :, -R: ], 
               # loss on Stream C only (the others are frozen anyways!)
         }
 
