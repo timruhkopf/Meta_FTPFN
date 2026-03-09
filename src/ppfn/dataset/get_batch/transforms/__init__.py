f37480d86be60201edfbfd48b46543bfc8e6297e
timruhkopf <timruhkopf@gmail.com>
2026-03-09 14:38:19 +0100

    fix:  import issue after refactor

---
+++ src/ppfn/dataset/get_batch/transforms/__init__.py
@@ -0,0 +1,7 @@
+from ppfn.dataset.get_batch.transforms.same_task import SameTaskTransform
+from ppfn.dataset.get_batch.transforms.bnn_interpolation import OutputInterpolationTransform
+
+__all__ = [
+    "SameTaskTransform",
+    "OutputInterpolationTransform",
+]
