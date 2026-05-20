f6fd84c5d0357dc4231b2f62046741d9a155f4b8
timruhkopf <timruhkopf@gmail.com>
2026-05-20 11:22:09 +0200

    feature:  Feature-wise attention + C has temporal cross attn on B post feature-wise attn. This is inspired by Tabpfn-v2's feature attention that was recycled by chronos-2 (they have lead-lag effects in their prior, which is basically our shift here)
    experiment:
    nll/C-A=0.4 after 2k steps (and declining steadily)
    experiment=prototype
    model=prototype_invariant
    model.model_class._target_=prototype.harmonic_restart.cross_feature.MultiStageTriHarmonicModel
    ~model.model_class.cross_attn_layer
    experiment_name=feat_attn
    trainer.steps=10
    trainer.epochs=10000
    dataset.dataset_class.warp=False
    dataset.dataset_class.scale=False
    dataset.dataset_class.shift=True
    dataset.dataset_class.batch_size=128
    dataset.dataset_class.share_unrelated=0.
    dataset.dataset_class.n_A=10
    trainer.trainer_class.warmup_epochs=500
    trainer.trainer_class.train_jointly=True
    callbacks.plot_heat.plot_every=250
    callbacks.plot_heat.start_plotting_epoch=500
    run_name=debug
    optimizer.lr=0.0001
    scheduler=onecylcelr
    trainer.trainer_class.use_amp=True
    trainer.trainer_class.verbose=True
    run_name=stacked_feat_attn

---
+++ src/prototype/harmonic_restart/__init__.py
@@ -0,0 +1,9 @@
+
+from prototype.harmonic_restart.harmonic_prior import InfiniteHarmonicsStream, HeatmapVisualizer
+
+# explicit exports
+__all__ = [
+    HeatmapVisualizer,
+    InfiniteHarmonicsStream
+
+]
